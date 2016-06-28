39ad13abc36f0a53c27256f8d0c3c3d829d29f5d
Diana Duong <dduong013@gmail.com>
2016-06-28 14:57:32 -0500

    added logic to getImages()

--- js/script.js
+++ js/script.js
@@ -10,31 +10,46 @@ angular
 	// that takes two parameters of $http and $sce
 	.controller('SearchController', function($http, $sce){ 
 
-// 		declare a variable controller syntax as vm to = this
+		// declare a variable controller syntax as vm to = this
+		var vm = this;
 
-// 		declare a function to getImages that takes the variable tag as an argument
-		
-// 			declare a variable called tag to capture user's input
-// 			declare a variable url to = endpoint : https://api.flickr.com/services/rest
 
-// 			declare a request object that holds the query parameters
 
-// 			var request = {
-// 			    method: 'flickr.photos.search',
-// 			    api_key: 24baf8bbf88eb2cfebccf7d27808233f,
-// 			    tags: search_tag,
-// 			    format: 'json',
-// 			    nojsoncallback: 1
-// 			}
+		// declare a function to getImages that takes the variable tag as an argument
+		vm.getImages = function(){
+			// 	declare a variable called searchtag to capture user's input
+			vm.searchTag = searchTag;
 
-// 			initialize the GET Request
-// 				method: GET
-// 				url: url
-// 				params: request
+			// declare a variable url to = endpoint : https://api.flickr.com/services/rest
+			vm.url = 'https://api.flickr.com/services/rest'
 
-// 			declare a then method that takes in a response
-// 			 the data key: response.data.photos.photo
+			// 	declare a request object that holds the query parameters
+			var request = {
+			    method: 'flickr.photos.search',
+			    api_key: '24baf8bbf88eb2cfebccf7d27808233f',
+			    tags: searchTag,
+			    format: 'json',
+			    nojsoncallback: 1
+			}
+
+			// initialize the GET Request
+			$http({
+				method: 'GET',
+				url: url,
+				params: request
+			})
+			// declare a then method that takes in a response
+			// the data key: response.data.photos.photo
+			.then(function(response){
+				vm.results = response.data.photos.photo;
+			},
+			function(response){
+				alert('error')
+			})
+		}
 
 	})
+
+
 
 
